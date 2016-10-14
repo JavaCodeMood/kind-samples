94585b07170438d0c553a65e2c4535118fb2ead2
自由鱼 <weiguo21@126.com>
2016-10-14 23:25:51 +0800

    netty demo

--- samples-netty/src/main/java/com/kind/samples/netty/client/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * Function:客户端. <br/>
- * @version  
- * @see 	 
- */
-package com.kind.samples.netty.client;
--- samples-netty/src/main/java/com/kind/samples/netty/server/package-info.java
+++
@@ -1,7 +0,0 @@
-
-/**
- * Function:服务器端. <br/>
- * @version  
- * @see 	 
- */
-package com.kind.samples.netty.server;
