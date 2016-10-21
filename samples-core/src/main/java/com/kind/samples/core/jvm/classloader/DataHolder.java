61bf5968a40ef5ddcc778543dc06265f8be16b46
自由鱼 <weiguo21@126.com>
2016-10-21 17:16:49 +0800

    classloader

---
+++ samples-core/src/main/java/com/kind/samples/core/jvm/classloader/DataHolder.java
@@ -0,0 +1,20 @@
+package com.kind.samples.core.jvm.classloader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 测试数据封装类
+ *
+ * @author: jian.cai@qunar.com
+ * @Date: 15/4/8 Time: 下午6:20
+ */
+public class DataHolder {
+    private static AtomicLong count = new AtomicLong();
+    public static List<String> list = new ArrayList<String>();
+    {
+        count.incrementAndGet();
+        System.out.println("current count value is " + count);
+    }
+}
